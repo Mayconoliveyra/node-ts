d222f77768ee91531be9c2b6da04c394c4ac2329
Mayconoliveyra <mayconbrito1998@hotmail.com>
2023-07-06 10:30:27 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import { server } from "./server/Server";
+
+server.listen(3333, () => console.log("Backend iniciado na porta [3333]"));
---
+++ src/server/Server.ts
@@ -0,0 +1,6 @@
+import express from "express";
+const server = express();
+
+server.get("/", (req, res) => res.status(200).json("Olá dev"));
+
+export { server };
