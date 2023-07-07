8773d6f23c219aeea836fbf5269efc90930283a8
Mayconoliveyra <mayconbrito1998@hotmail.com>
2023-07-07 20:34:47 -0300

    update

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,19 @@
 import { server } from './Server';
+import { Knex } from './Server/Database/Knex';
 
-server.listen(process.env.PORT || 3333, () =>
-  console.log(`Back-end iniciado na porta ${process.env.PORT || 3333}.`),
-);
+const startServer = () => {
+  server.listen(process.env.PORT || 3333, () => {
+    console.log(`App rodando na porta ${process.env.PORT || 3333}`);
+  });
+};
+
+if (process.env.NODE_ENV !== 'dev') {
+  Knex.migrate
+    .latest()
+    .then(() => {
+      startServer();
+    })
+    .catch(console.log);
+} else {
+  startServer();
+}
