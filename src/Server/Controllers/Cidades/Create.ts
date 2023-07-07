204ded1f6dfd77dec37a20d9b414c85c696fb26f
Mayconoliveyra <mayconbrito1998@hotmail.com>
2023-07-07 17:35:05 -0300

    upp

--- src/Server/Controllers/Cidades/Create.ts
+++ src/Server/Controllers/Cidades/Create.ts
@@ -3,6 +3,7 @@ import { StatusCodes } from 'http-status-codes';
 import * as yup from 'yup';
 
 import { ICidade } from '../../Database/Models/Cidade';
+import { CidadesProvider } from '../../Database/Providers/Cidades';
 import { validation } from '../../Shared/Middleware/Validation';
 
 interface IBodyProps extends Omit<ICidade, 'id'> {}
@@ -10,13 +11,21 @@ interface IBodyProps extends Omit<ICidade, 'id'> {}
 export const createValidation = validation((getSchema) => ({
   body: getSchema<IBodyProps>(
     yup.object().shape({
-      nome: yup.string().required().min(3),
+      nome: yup.string().required().min(3).max(150),
     }),
   ),
 }));
 
 export const create = async (req: Request<{}, {}, IBodyProps>, res: Response) => {
-  console.log('[Create] Não implementado!');
+  const result = await CidadesProvider.create(req.body);
 
-  return res.status(StatusCodes.CREATED).json(1);
+  if (result instanceof Error) {
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      errors: {
+        default: result.message,
+      },
+    });
+  }
+
+  return res.status(StatusCodes.CREATED).json(result);
 };
--- tests/jest.setup.ts
+++ tests/jest.setup.ts
@@ -1,5 +1,14 @@
 import supertest from 'supertest';
 
 import { server } from '../src/Server';
+import { Knex } from '../src/Server/Database/Knex';
+
+beforeAll(async () => {
+  await Knex.migrate.latest();
+});
+
+afterAll(async () => {
+  await Knex.destroy();
+});
 
 export const testServer = supertest(server);
