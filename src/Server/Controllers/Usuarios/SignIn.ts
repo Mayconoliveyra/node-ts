cd5f572185c29f3d1de3f17241d8bea304e561f4
Mayconoliveyra <mayconbrito1998@hotmail.com>
2023-07-08 11:27:42 -0300

    update JWT

--- src/Server/Controllers/Usuarios/SignIn.ts
+++ src/Server/Controllers/Usuarios/SignIn.ts
@@ -5,6 +5,7 @@ import * as yup from 'yup';
 import { IUsuario } from '../../Database/Models/Usuario';
 import { UsuariosProvider } from '../../Database/Providers/Usuarios';
 import { validation } from '../../Shared/Middleware/Validation';
+import { JWTService } from '../../Shared/Services/JWTService';
 import { PasswordCrypto } from '../../Shared/Services/PasswordCrypto';
 
 interface IBodyProps extends Omit<IUsuario, 'id' | 'nome'> {}
@@ -21,8 +22,8 @@ export const signInValidation = validation((getSchema) => ({
 export const signIn = async (req: Request<{}, {}, IBodyProps>, res: Response) => {
   const { email, senha } = req.body;
 
-  const result = await UsuariosProvider.getByEmail(email);
-  if (result instanceof Error) {
+  const usuario = await UsuariosProvider.getByEmail(email);
+  if (usuario instanceof Error) {
     return res.status(StatusCodes.UNAUTHORIZED).json({
       errors: {
         default: 'Email ou senha são inválidos',
@@ -30,7 +31,7 @@ export const signIn = async (req: Request<{}, {}, IBodyProps>, res: Response) =>
     });
   }
 
-  const passwordMatch = await PasswordCrypto.verifyPassword(senha, result.senha);
+  const passwordMatch = await PasswordCrypto.verifyPassword(senha, usuario.senha);
   if (!passwordMatch) {
     return res.status(StatusCodes.UNAUTHORIZED).json({
       errors: {
@@ -38,6 +39,15 @@ export const signIn = async (req: Request<{}, {}, IBodyProps>, res: Response) =>
       },
     });
   } else {
-    return res.status(StatusCodes.OK).json({ accessToken: 'teste.teste.teste' });
+    const accessToken = JWTService.sign({ uid: usuario.id });
+    if (accessToken === 'JWT_SECRET_NOT_FOUND') {
+      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+        errors: {
+          default: 'Erro ao gerar o token de acesso',
+        },
+      });
+    }
+
+    return res.status(StatusCodes.OK).json({ accessToken });
   }
 };
