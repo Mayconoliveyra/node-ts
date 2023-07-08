c16152bf3a7ab008d83246b765250242f41183c8
Mayconoliveyra <mayconbrito1998@hotmail.com>
2023-07-08 09:50:23 -0300

    up m

--- src/Server/index.ts
+++ src/Server/index.ts
@@ -1,4 +1,8 @@
 import express from 'express';
+import fs from 'fs';
+import moment from 'moment';
+import morgan from 'morgan-body';
+import path from 'path';
 
 import 'dotenv/config';
 import './Shared/Services/TranslationsYup';
@@ -6,6 +10,20 @@ import './Shared/Services/TranslationsYup';
 import { router } from './Routes';
 
 const server = express();
+
+const log = fs.createWriteStream(
+  path.join(__dirname, '..', './logs', `express-${moment().format('YYYY-MM-DD')}.log`),
+  {
+    flags: 'a',
+  },
+);
+
+morgan(server, {
+  noColors: true,
+  stream: log,
+  prettify: true,
+  filterParameters: ['senha', 'email', 'accessToken'],
+});
 
 server.use(express.json());
 server.use(router);
